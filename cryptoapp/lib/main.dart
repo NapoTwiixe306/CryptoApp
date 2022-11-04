b09636ba8ca5036f7f123b00cbac85f184084cf3
Julien <62757437+NapoTwiixe306@users.noreply.github.com>
2022-11-04 10:10:01 +0100

    debut app

--- cryptoapp/lib/main.dart
+++ cryptoapp/lib/main.dart
@@ -1,34 +1,69 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(const MaterialApp(home: Home()));
+void main() => runApp(const MaterialApp(
+      home: CryptoExt(),
+    ));
 
-class Home extends StatelessWidget {
-  const Home({super.key});
+class CryptoExt extends StatelessWidget {
+  const CryptoExt({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey[850],
+      backgroundColor: Colors.grey[900],
       appBar: AppBar(
-        title: const Text('CryptoExtension'),
+        title: const Text("CryptoExtension"),
         centerTitle: true,
         backgroundColor: Colors.grey[850],
+        elevation: 0.0,
       ),
-      body: Center(
-        child: Text(
-          'CryptoExtension',
-          style: TextStyle(
-            fontSize: 20.0,
-            fontWeight: FontWeight.bold,
-            letterSpacing: 2.0,
-            color: Colors.red[700],
-          ),
+      body: Padding(
+        padding: const EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text(
+              'Name',
+              style: TextStyle(
+                color: Colors.grey[600],
+                letterSpacing: 2.0,
+              ),
+            ),
+            const SizedBox(
+              height: 10.0,
+            ),
+            Text(
+              'Julien Milants',
+              style: TextStyle(
+                color: Colors.amberAccent[400],
+                letterSpacing: 2.0,
+                fontSize: 20.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(height: 10.0),
+            Text(
+              'Current Code Hours',
+              style: TextStyle(
+                color: Colors.grey[600],
+                letterSpacing: 2.0,
+              ),
+            ),
+            const SizedBox(
+              height: 10.0,
+            ),
+            Text(
+              '+ 20 000 hours',
+              style: TextStyle(
+                color: Colors.amberAccent[400],
+                letterSpacing: 2.0,
+                fontSize: 20.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(height: 30.0),
+          ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: null,
-        backgroundColor: Colors.red[600],
-        child: const Text('click'),
       ),
     );
   }
--- cryptoapp/test/widget_test.dart
+++ cryptoapp/test/widget_test.dart
@@ -8,12 +8,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:cryptoapp/main.dart';
+//import 'package:cryptoapp/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(Home());
+    // await tester.pumpWidget(CryptoExt());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
