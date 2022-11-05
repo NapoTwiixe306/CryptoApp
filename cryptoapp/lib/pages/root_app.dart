29611d66fbff769a4f25318c18b390ecf069c313
Julien <62757437+NapoTwiixe306@users.noreply.github.com>
2022-11-05 16:23:45 +0100

    start && modifs app

--- cryptoapp/lib/pages/root_app.dart
+++ cryptoapp/lib/pages/root_app.dart
@@ -10,41 +10,85 @@ class RootApp extends StatefulWidget {
 }
 
 class _RootAppState extends State<RootApp> {
+  int pageIndex = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: getAppBar(),
       //backgroundColor: Colors.black,
       bottomNavigationBar: getFooter(),
     );
   }
 
+  getAppBar() {
+    if (pageIndex == 0) {
+      return AppBar(
+        backgroundColor: appBarColor,
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            SvgPicture.asset(
+              "assets/images/camera_icon.svg",
+              width: 30,
+            ),
+            const Text(
+              "CryptoExtension",
+              style: TextStyle(
+                fontSize: 25,
+              ),
+            ),
+            SvgPicture.asset(
+              "assets/images/message_icon.svg",
+              width: 30,
+            ),
+          ],
+        ),
+      );
+    }
+  }
+
   Widget getFooter() {
     List bottomItems = [
-      "assets/images/home_active_icon.svg",
-      "assets/images/search_active_icon.svg",
-      "assets/images/upload_active_icon.svg",
-      "assets/images/upload_active_icon.svg",
-      "assets/images/account_active_icon.svg",
+      pageIndex == 0
+          ? "assets/images/home_active_icon.svg"
+          : "assets/images/home_icon.svg",
+      pageIndex == 1
+          ? "assets/images/search_active_icon.svg"
+          : "assets/images/search_icon.svg",
+      pageIndex == 2
+          ? "assets/images/upload_active_icon.svg"
+          : "assets/images/upload_icon.svg",
+      pageIndex == 3
+          ? "assets/images/account_active_icon.svg"
+          : "assets/images/account_icon.svg",
     ];
     return Container(
       width: double.infinity,
       height: 80,
-      decoration: const BoxDecoration(
-        color: appFooterColor,
-      ),
+      decoration: const BoxDecoration(color: appFooterColor),
       child: Padding(
         padding:
             const EdgeInsets.only(left: 20, right: 20, bottom: 20, top: 15),
         child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: List.generate(bottomItems.length, (index) {
-            return SvgPicture.asset(
-              bottomItems[index],
-              width: 27,
-            );
+            return InkWell(
+                onTap: () {
+                  selectedTab(index);
+                },
+                child: SvgPicture.asset(
+                  bottomItems[index],
+                  width: 27,
+                ));
           }),
         ),
       ),
     );
+  }
+
+  selectedTab(index) {
+    setState(() {
+      pageIndex = index;
+    });
   }
 }
