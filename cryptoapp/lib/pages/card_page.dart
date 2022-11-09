01888845463e5f08012bc57e8ceb3b28eaacde0a
Julien <62757437+NapoTwiixe306@users.noreply.github.com>
2022-11-09 17:42:09 +0100

    add card_page

--- cryptoapp/lib/pages/card_page.dart
+++ cryptoapp/lib/pages/card_page.dart
@@ -1,8 +1,11 @@
+import 'package:cryptoapp/data_json/card_json.dart';
+import 'package:cryptoapp/data_json/card_operations_json.dart';
 import 'package:cryptoapp/pages/root_app.dart';
 import 'package:cryptoapp/theme/colors.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter_icons/flutter_icons.dart';
 
 class CardPage extends StatefulWidget {
   const CardPage({super.key});
@@ -56,22 +59,272 @@ class _CardPageState extends State<CardPage> {
   }
 
   Widget getBody() {
+    var size = MediaQuery.of(context).size;
+    final PageController controller = PageController();
     return SingleChildScrollView(
       child: Column(
-        children: const [
-          Align(
-            alignment: Alignment.center,
-            child: Text(
-              "Card Page",
-              style: TextStyle(
-                color: primary,
-                fontSize: 25,
-                fontWeight: FontWeight.bold,
-              ),
+        children: [
+          const SizedBox(
+            height: 10,
+          ),
+          SizedBox(
+            width: double.infinity,
+            height: 240,
+            child: PageView(
+              controller: controller,
+              children: List.generate(cardLists.length, (index) {
+                return getCard(
+                  cardLists[index]['amount'],
+                  cardLists[index]['currency'],
+                  cardLists[index]['card_number'],
+                  cardLists[index]['valid_date'],
+                  cardLists[index]['bg_color'],
+                );
+              }),
+            ),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          Container(
+            width: double.infinity,
+            decoration: BoxDecoration(
+              color: white,
+              borderRadius: BorderRadius.circular(15),
+              boxShadow: [
+                BoxShadow(
+                  color: grey.withOpacity(0.1),
+                  spreadRadius: 10,
+                  blurRadius: 10,
+                ),
+              ],
+            ),
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(top: 10, bottom: 10),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Flexible(
+                        child: Container(
+                          width: size.width / 2,
+                          height: 55,
+                          decoration: const BoxDecoration(
+                            border: Border(
+                              bottom: BorderSide(
+                                color: primary,
+                                width: 3.5,
+                              ),
+                            ),
+                          ),
+                          child: const Center(
+                            child: Text(
+                              "Operation",
+                              style: TextStyle(
+                                fontSize: 15,
+                                color: primary,
+                                fontWeight: FontWeight.w600,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                      Flexible(
+                        child: Container(
+                          width: size.width / 2,
+                          height: 55,
+                          decoration: BoxDecoration(
+                            border: Border(
+                              bottom: BorderSide(
+                                  color: black.withOpacity(0.05), width: 1),
+                            ),
+                          ),
+                          child: Center(
+                            child: Text(
+                              "Historique",
+                              style: TextStyle(
+                                fontSize: 15,
+                                color: primary.withOpacity(0.5),
+                                fontWeight: FontWeight.w600,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(height: 25),
+                Column(
+                  children: List.generate(
+                    cardOperations.length,
+                    (index) {
+                      return Padding(
+                        padding: const EdgeInsets.only(
+                            left: 20, right: 20, bottom: 20),
+                        child: Container(
+                          width: double.infinity,
+                          decoration: BoxDecoration(
+                            color: white,
+                            borderRadius: BorderRadius.circular(15),
+                            boxShadow: [
+                              BoxShadow(
+                                color: grey.withOpacity(0.1),
+                                spreadRadius: 10,
+                                blurRadius: 10,
+                                // changes positions of shadow
+                              ),
+                            ],
+                          ),
+                          child: Padding(
+                            padding: const EdgeInsets.all(18.0),
+                            child: Row(
+                              children: [
+                                Container(
+                                  width: 40,
+                                  height: 40,
+                                  decoration: BoxDecoration(
+                                    color: secondary.withOpacity(0.3),
+                                    borderRadius: BorderRadius.circular(12),
+                                  ),
+                                  child: Center(
+                                    child: Icon(
+                                      cardOperations[index]['icon'],
+                                      color: primary,
+                                      size: 20,
+                                    ),
+                                  ),
+                                ),
+                                const SizedBox(
+                                  width: 15,
+                                ),
+                                Text(
+                                  cardOperations[index]['title'],
+                                  style: const TextStyle(
+                                      fontSize: 15,
+                                      fontWeight: FontWeight.w600),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      );
+                    },
+                  ),
+                ),
+                const SizedBox(
+                  height: 30,
+                )
+              ],
             ),
           ),
         ],
       ),
+    );
+  }
+
+  getCard(amount, currency, cardNumber, validDate, bgColor) {
+    var size = MediaQuery.of(context).size;
+    return Column(
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(top: 5),
+              child: Text(
+                currency,
+                style: const TextStyle(
+                  fontSize: 17,
+                  color: black,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            const SizedBox(
+              width: 5,
+            ),
+            Text(
+              amount,
+              style: const TextStyle(
+                fontSize: 35,
+                color: black,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ],
+        ),
+        const SizedBox(
+          height: 15,
+        ),
+        Container(
+          width: size.width * 0.85,
+          height: 170,
+          decoration: BoxDecoration(
+              color: bgColor, borderRadius: BorderRadius.circular(12)),
+          child: Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Icon(
+                      Feather.credit_card,
+                      size: 30,
+                      color: white.withOpacity(0.3),
+                    ),
+                    const SizedBox(
+                      height: 15,
+                    ),
+                    Text(
+                      cardNumber,
+                      style: TextStyle(
+                          color: white.withOpacity(0.8),
+                          fontSize: 20,
+                          wordSpacing: 15),
+                    )
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "VALID DATE",
+                          style: TextStyle(
+                              color: white.withOpacity(0.3), fontSize: 12),
+                        ),
+                        const SizedBox(
+                          height: 4,
+                        ),
+                        Text(
+                          validDate,
+                          style: const TextStyle(
+                            color: white,
+                            fontSize: 13,
+                          ),
+                        ),
+                      ],
+                    ),
+                    Image.asset(
+                      "assets/images/master_card_logo.png",
+                      width: 50,
+                    )
+                  ],
+                )
+              ],
+            ),
+          ),
+        )
+      ],
     );
   }
 }
