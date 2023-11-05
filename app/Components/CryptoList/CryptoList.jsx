09077137f1e1e43a802bf896d8a8a891c0221015
Julien <62757437+NapoTwiixe306@users.noreply.github.com>
2023-11-05 20:23:24 +0100

    j'ai enfin le bon icon

---
+++ app/Components/CryptoList/CryptoList.jsx
@@ -0,0 +1,15 @@
+import {Text, View } from 'react-native'
+import React from 'react'
+import BottomBar from '../../Widget/Navbar/BottomBar'
+
+export default function CryptoList() {
+  return (
+    <>
+      <BottomBar/>
+      <View>
+        <Text>CryptoList</Text>
+      </View>
+    </>
+  )
+}
+
--- app/Widget/Navbar/BottomBar.jsx
+++ app/Widget/Navbar/BottomBar.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, TouchableOpacity } from 'react-native';
+import { View, Text } from 'react-native';
 import { Link } from 'expo-router';
 import { FontAwesome } from '@expo/vector-icons';
 import styles from './BottomBarStyle';
@@ -13,10 +13,10 @@ export default function BottomBar() {
           <Text style={styles.tabText}>Home</Text>
         </View>
       </Link>
-      <Link style={styles.tab} href="/Components/Contact/Contact">
+      <Link style={styles.tab} href="/Components/CryptoList/CryptoList">
         <View style={{flex: 1, alignItems: "center", justifyContent: "center"}}>
-          <FontAwesome name="phone" size={24} style={styles.icon} />
-          <Text style={styles.tabText}>Contact</Text>
+          <FontAwesome name="btc" size={24} style={styles.icon} />
+          <Text style={styles.tabText}>Crypto</Text>
         </View>
       </Link>
       <Link style={styles.tab} href="/Components/Login/Login">
